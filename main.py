038ee132788ae561adfe56101abd1f55a692c74e
muhibbx5 <muhibbali56@gmail.com>
2024-11-16 12:09:02 +0500

    com

---
+++ main.py
@@ -0,0 +1,8 @@
+# Ask the user for the number
+n = int(input("Enter the number: "))
+
+# Calculate 2 to the power of n
+sum = 2 ** n
+
+# Print the result
+print("2 raised to the power of", n, "is", sum)
